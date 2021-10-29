df1769be326aa2e3c916715052a8544bd9c540ad
sherameing <bayadogchristopher18@gmail.com>
2021-10-29 16:07:22 -0400

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+
+const routes: Routes = [
+  {path: "profile", component: ProfileComponent},
+  {path: "gallery", component: GalleryComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "**", redirectTo: "profile"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+  
